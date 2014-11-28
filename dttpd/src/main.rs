f4ffabd35b07b3fc932b220fdb5e474cedec2227
kurotetsuka <kurotetsuka@gmail.com>
2014-11-27 20:31:55 -0500

    just little tests happening

--- dttpd/src/main.rs
+++ dttpd/src/main.rs
@@ -5,6 +5,7 @@
 extern crate dttp;
 
 // local uses
+use dttp::mote::Mote;
 use dttp::hub::Hub;
 
 // entry function
@@ -12,6 +13,9 @@ fn main(){
 	println!("test \
 		test \
 		test");
+
+	let mote = Mote::new_test();
+	println!( "mote: {}", mote);
 
 	let hub = Hub::new();
 	hub.say_hi()}
