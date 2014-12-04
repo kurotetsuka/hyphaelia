508f9ce67e41a8fc511aaa32f9146d2308b1f6c6
kurotetsuka <kurotetsuka@gmail.com>
2014-12-03 19:28:48 -0500

    messed around with the jquery stuff

--- web/static/js/back.js
+++ web/static/js/back.js
@@ -1,17 +1,18 @@
 
-function load_overview(){
-	var dest = $( "#content");
+function load_mote(){
+	//var dest = $( "#content");
 	var request = location.protocol + "//";
 	request += location.hostname + ":7860";
-	request += "/";
-	dest.html( "loading...");
-	$.get( request, load_overview_content)
-		.fail( load_overview_exception);}
+	request += "/mote/304325";
+	//dest.html( "loading...");
+	$.get( request, load_mote_content)
+		.fail( load_mote_exception);}
 
-function load_overview_content( data){
-	//console.log( data);
-	$( "#content").html(
-		"<pre><code>" + JSON.stringify( data) + "</code></pre>");}
-function load_overview_exception( exception){
-	var dest = $( "#content");
-	dest.html( "connection failed");}
+function load_mote_content( data){
+	console.log( data);
+	/*$( "#content").html(
+		"<pre><code>" + JSON.stringify( data) + "</code></pre>");*/}
+function load_mote_exception( exception){
+	console.log( "connection failed");
+	/*var dest = $( "#content");
+	dest.html( "connection failed");*/}
