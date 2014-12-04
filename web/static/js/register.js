ae25abc73fc285b749a9217ca7b05c23245f02f3
Jordan <jojosali@hotmail.com>
2014-12-03 19:01:00 -0500

    added register and login js files - incomplete

---
+++ web/static/js/register.js
@@ -0,0 +1,20 @@
+
+var username;
+var password;
+var email;
+var keyID;
+
+$(function register()) {
+
+	username = document.getElementByID('register-username');
+	password = document.getElementByID('register-password');
+	email = document.getElementByID('register-email');
+	//keyID = ;
+
+	function validateForm(){
+		var x = document.forms["register-form"]["register-username"].value;
+		
+	}
+	//add to db 
+
+}
