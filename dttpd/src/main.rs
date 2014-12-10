f29b77c26209473fcc3ce5e517a23f2820942be1
kurotetsuka <kurotetsuka@gmail.com>
2014-12-10 18:49:52 -0500

    setting up a bootstrap list

--- dttpd/src/main.rs
+++ dttpd/src/main.rs
@@ -11,6 +11,9 @@ extern crate serialize;
 // library uses
 use std::hash;
 use std::io::net::ip::{ SocketAddr, ToSocketAddr};
+use std::io::BufferedReader;
+use std::path::Path;
+use std::io::fs::File;
 use std::num;
 use std::os;
 
@@ -36,31 +39,27 @@ fn main(){
 	hub.say_hi();
 
 	//add bootstrap remotes
-	let bs_list = [
-		("localhost", 8960),
-		("localhost", 8961),
-		("localhost", 8962),
-		("localhost", 8963),];
+	let bs_list = load_bootstrap_list( "config/dev.bs");
+	println!( "bs list: {}", bs_list);
 	for &bs in bs_list.iter() {
-		let bs : Option<SocketAddr> = bs.to_socket_addr().ok();
-		if bs.is_none() { continue;}
-		hub.add_remote( bs.unwrap());}
+		hub.add_remote( bs);}
 
 	let mote = test_mote();
 	let mote_hash = hash::hash( &mote);
-	println!( "generated test mote: {:x}::{}", mote_hash, mote);
+	println!( "generated test mote: {:x} :: {}", mote_hash, mote);
 	//println!( "mote json: {}", 
 	//	serialize::json::encode( &mote.to_msg()));
 	hub.add_mote( mote);
 
 	// launch_hub
-	hub.launch();
-}
+	hub.launch();}
 
 fn test_mote() -> Mote {
+	// get rng
 	let rng = std::rand::OsRng::new();
 	let mut rng = rng.unwrap();
 
+	// create new auth
 	let auth = Auth::new(
 		Some( "kurotetsuka".to_string()),
 		None,
@@ -68,9 +67,11 @@ fn test_mote() -> Mote {
 		Some( 0x0a1a20c0));
 	//println!( "auth: {}", auth);
 
+	// create fake keypair
 	let keypair = key::keygen_fake( &mut rng);
 	let ( sec_key, _pub_key ) = keypair;
 
+	// create mote
 	let mut mote = Mote::new_text(
 		"test test :)".to_string(),
 		dttp::mote::Class::Markdown,
@@ -80,3 +81,36 @@ fn test_mote() -> Mote {
 	mote.sign( &auth, &sec_key);
 
 	return mote;}
+
+fn load_bootstrap_list( bs_filename: &str) -> Vec<SocketAddr> {
+	let mut bs_list : Vec<SocketAddr> = Vec::new();
+
+	//for each line in file
+	let lines = read_lines( bs_filename);
+	for line in lines.iter() {
+		let line = line.as_slice().trim();
+		// ignore comments
+		if line.char_at( 0) == '#' { continue;}
+		// parse address
+		let addr : Option<SocketAddr> = line.to_socket_addr().ok();
+		if addr.is_none() { continue;}
+		// push address
+		bs_list.push( addr.unwrap());}
+
+	// done
+	return bs_list;}
+
+fn read_lines( filename : &str) -> Vec<String> {
+	// open the file
+	let path = Path::new( filename);
+	let file = File::open( &path);
+	let mut reader = BufferedReader::new( file);
+	// read the file
+	let mut result : Vec<String> = Vec::new();
+	for line_result in reader.lines() {
+		if line_result.is_err() { continue;}
+		let line = line_result.unwrap();
+		// strip the newline chars
+		let line_stripped = line.as_slice().trim_chars('\n');
+		result.push( String::from_str( line_stripped));}
+	return result;}
